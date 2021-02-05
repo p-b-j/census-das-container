f09dd7e2f2a2839fd8383b0a8ea76a22f89fd325
Porter Jones <pbjones@cs.washington.edu>
2021-02-04 17:16:41 -0800

    Remove unnecessary Spark tmp configuration from start_das.sh

--- start_das.sh
+++ start_das.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+# Env setup
 export DAS_VERSION="Standalone"
 export GRB_LICENSE_FILE="${HOME}/gurobi.lic"
 export GUROBI_HOME="/usr/local/gurobi911/linux64"
@@ -10,12 +11,6 @@ export PYSPARK_PYTHON="/usr/local/anaconda3/bin/python3.6"
 export PYTHONPATH="/usr/local/spark/spark-2.4.7-bin-hadoop2.7/python:"
 export SPARK_HOME="/usr/local/spark/spark-2.4.7-bin-hadoop2.7"
 
-mkdir -p singularity_tmp/spark_tmp
-mkdir -p ~/spark-2.4.7-bin-hadoop2.7/conf/
-
-SPARK_CONF_LINE="spark.local.dir ${PWD}/singularity_tmp/spark_tmp"
-SPARK_CONF_FILE="${HOME}/spark-2.4.7-bin-hadoop2.7/conf/spark-defaults.conf"
-touch $SPARK_CONF_FILE
-grep -qxF $SPARK_CONF_LINE $SPARK_CONF_FILE || echo $SPARK_CONF_LINE > $SPARK_CONF_FILE
+# Run standalone config
 cd census2020-das-e2e
 ./run_1940_standalone.sh
